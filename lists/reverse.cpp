2d13c86990ef76b3ff0e621e7bd49edc824e86a9
yashsvi <141433982+yssvi72@users.noreply.github.com>
2024-02-26 22:02:54 +0530

    Create reverse.cpp

---
+++ lists/reverse.cpp
@@ -0,0 +1,14 @@
+//This method can be used to reverse a list completely.
+
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+int main()
+{
+    list<int> l{1,2,3,4,5};
+    
+    l.reverse();
+    /* now the list becomes 5,4,3,2,1 */
+}
